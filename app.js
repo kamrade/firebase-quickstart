6be7834ea15652a13548980c2c9cee0c06414923
Denis Michailov <kamrad.reg@gmail.com>
2016-09-18 12:12:48 +1000

    getting started with firebase storage

--- app.js
+++ app.js
@@ -8,55 +8,42 @@
         apiKey: "AIzaSyB0cz9ohtYiauNz6e7FxiLCR793cl_URFY",
         authDomain: "web-quickstart-d5469.firebaseapp.com",
         databaseURL: "https://web-quickstart-d5469.firebaseio.com",
-        storageBucket: "",
+        storageBucket: "web-quickstart-d5469.appspot.com",
         messagingSenderId: "589127434951"
     };
     firebase.initializeApp(config);
 
     // cache DOM
-    const txtEmail = document.getElementById('txtEmail');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignUp = document.getElementById('btnSignUp');
-    const btnLogout = document.getElementById('btnLogout');
+    var uploader = document.getElementById('uploader');
+    var fileButton = document.getElementById('fileButton');
 
-    // Add login event
-    btnLogin.addEventListener('click', e => {
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-        // sign in
-        const promise = auth.signInWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message));
+    // listener
+    fileButton.addEventListener('change', function(e){
+        // get file
+        var file = e.target.files[0];
+
+        // create a storage ref
+        var storageRef = firebase.storage().ref('sweet_gifs/' + file.name);
+
+        // upload file
+        var task = storageRef.put(file);
+
+        // update progress bar
+        task.on('state_changed',
+
+            function progress(snapshot){
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                uploader.value = percentage;
+            },
+            function error(err){
+                console.log('error');
+            },
+            function complete(){
+                console.log('complete');
+            }
+
+        )
+
     });
-
-    // Add sign Up event
-    btnSignUp.addEventListener('click', e => {
-        // Get email and pass
-        // TODO: check for real email
-        const email = txtEmail.value;
-        const pass = txtPassword.value;
-        const auth = firebase.auth();
-        // sign in
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise
-            .catch(e => console.log(e.message));
-    });
-
-    btnLogout.addEventListener('click', e => {
-        firebase.auth().signOut();
-    })
-
-    // Add a real time listener
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if(firebaseUser){
-            console.log(firebaseUser);
-            btnLogout.classList.remove('hide');
-        } else {
-            console.log('not logged in');
-            btnLogout.classList.add('hide');
-        }
-    });
-
 
 }());
